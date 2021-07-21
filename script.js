6575b63128483a7d291018a1ae22cae396375108
Israel S. Nazareth <israel.fab@hotmail.com>
2021-07-21 19:11:40 -0300

    Requisito 18 - Adicionando funcao ao botao submit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const buttonLogin = document.getElementById('buttonLogin');
 const login = document.getElementById('login');
 const password = document.getElementById('password');
+const submitButton = document.getElementById('submit-btn');
+const checkboxAgree = document.getElementById('agreement');
 
 function loginAlert(event) {
   if (login.value !== 'tryber@teste.com' && password.value !== '123456') {
@@ -9,6 +11,12 @@ function loginAlert(event) {
   } else {
     alert('Olá, Tryber!');
   }
-}
+} buttonLogin.addEventListener('click', loginAlert);
 
-buttonLogin.addEventListener('click', loginAlert);
+function submitEnable() {
+  if (checkboxAgree.checked === true) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+} checkboxAgree.addEventListener('click', submitEnable);
